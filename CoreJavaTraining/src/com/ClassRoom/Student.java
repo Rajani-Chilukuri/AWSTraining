73d0b0698bf41c558ea50aedcdac5a7f5dfbec95
rajchilu <rajchilu@LIN51016531.corp.capgemini.com>
2021-10-14 15:18:21 +0530

    Second Commit

---
+++ CoreJavaTraining/src/com/ClassRoom/Student.java
@@ -0,0 +1,23 @@
+package com.ClassRoom;
+
+public class Student {
+		private int id;
+		private String Name;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return Name;
+		}
+		public void setName(String name) {
+			Name = name;
+		}
+		
+
+	}
+
+
+
